563c8d8ed402a15d7af68f4dfa468457465d7b73
HelloWorld <hello@example.com>
2022-01-05 23:00:15 +0530

    add prev and next button

--- src/pages/RegisterPageBusiness/index.js
+++ src/pages/RegisterPageBusiness/index.js
@@ -1,23 +1,52 @@
 import React from "react";
 import { Steps, StepsProvider, useSteps } from "react-step-builder";
 import Valuation from "@components/Valuation/index.js";
-import FinishRegister from "@components/FinishRegister/index.js"
+import FinishRegister from "@components/FinishRegister/index.js";
+import { Button } from "@material-ui/core";
 
-const RegisterPageBusiness = () => {
-  const { next, prev } = useSteps();
+const MySteps = () => {
+  const { next, prev, total, current } = useSteps();
   return (
-    <StepsProvider>
+    <>
       <Steps>
         <div>
-        <FinishRegister />
+          <FinishRegister />
         </div>
         <div>
-         <FinishRegister />
+          <FinishRegister />
         </div>
         <div>
           <Valuation />
         </div>
       </Steps>
+      <Button
+        type="button"
+        color="primary"
+        variant="contained"
+        className="my-2 btn-primary py-3"
+        onClick={prev}
+        disabled={current === 1}
+      >
+        Previous
+      </Button>
+      <Button
+        type="button"
+        color="primary"
+        variant="contained"
+        className="my-2 btn-primary py-3"
+        onClick={next}
+        disabled={current === total}
+      >
+        Next
+      </Button>
+    </>
+  );
+};
+
+const RegisterPageBusiness = () => {
+  return (
+    <StepsProvider>
+      <MySteps />
     </StepsProvider>
   );
 };
